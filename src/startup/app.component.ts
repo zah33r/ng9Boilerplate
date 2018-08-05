e5d02cb905d1c2e4e2112ce6315b21eaa7320633
Zaheer uddin Babar <zaheer_sagitarius@hotmail.com>
2018-08-05 08:23:04 -0400

    display version number

--- src/startup/app.component.ts
+++ src/startup/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppConfig } from '../constants/app-config';
 
 @Component({
     selector: 'app-root',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     routes: AppRouterLink[] = [];
     title = 'Angular';
+    version = AppConfig.AppVersion;
 
     ngOnInit(): void {
         this.loadRoutes();
