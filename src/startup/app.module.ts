a0b15520b3523e293f0a3aebdcb0475d20f47784
Zaheeruddin Babar <zaheer_sagitarius@hotmail.com>
2020-05-27 04:23:05 +0500

    enabled ivy, removed legacy package

--- src/startup/app.module.ts
+++ src/startup/app.module.ts
@@ -3,7 +3,6 @@ import { AppComponent } from './app.component';
 import { RoutingModule } from '../modules/routing/routing.module';
 import { AppMaterialModule } from '../modules/app-material/app-material.module';
 import { AppAgGridModule } from '../modules/base/components/app-ag-grid/app-ag-grid.module';
-import { FileUploadModule } from 'ng2-file-upload';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { CacheModule } from '../modules/cache/cache.module';
 import { ToolbarComponent } from '../modules/feature/toolbar/toolbar.component';
@@ -13,7 +12,7 @@ import { CoreModule } from '../modules/core/core.module';
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, ToolbarComponent],
-    imports: [CoreModule, RoutingModule, AppMaterialModule, AppAgGridModule, FileUploadModule, FlexLayoutModule, CacheModule, HttpModule],
+    imports: [CoreModule, RoutingModule, AppMaterialModule, AppAgGridModule, FlexLayoutModule, CacheModule, HttpModule],
     entryComponents: [],
     bootstrap: [AppComponent],
 })
